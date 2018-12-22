ce50d9e977ff3600db3ae07ed567cec4534225e6
Alberto Vargas <a.vargasmarte@gmail.com>
2018-12-21 19:07:43 -0500

    modified server.js

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+
+app.use(express.static("client/build"));
+
 // Add routes to our API
 app.use("/api", routes);
 
